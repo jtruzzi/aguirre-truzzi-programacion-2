9154a71517f64a91cd285d0aa4a54087344fead8
Julio Truzzi <julio.truzzi@gmail.com>
2021-10-28 09:41:31 -0300

    Agregar mas ejercicio de recursividad

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 import json
 
 
-def ejercicio1():
+def regex_1():
     print("--------------")
     print("Ingrese la matricula de aerolinea:")
     print('')
@@ -16,7 +16,7 @@ def ejercicio1():
     print('')
 
 
-def ejercicio2():
+def regex_2():
     print("--------------")
     print("Ingrese un numero menor a 1900:")
     print('')
@@ -30,7 +30,7 @@ def ejercicio2():
     print('')
 
 
-def ejercicio3():
+def intercambio_1():
     print("--------------")
     print("Ingrese el nombre de la estacion:")
     print('')
@@ -54,7 +54,7 @@ def ejercicio3():
     print('')
 
 
-def ejercicio4():
+def intercambio_2():
     archivo = open('estaciones.json',)
     estaciones = json.load(archivo)
     ordenadas = sorted(estaciones, key=lambda estacion: sum(
@@ -65,7 +65,7 @@ def ejercicio4():
     archivo.close()
 
 
-def ejercicio5():
+def recursividad_1():
     def convertir(numero):
         if len(numero) == 1:
             return numero
@@ -82,6 +82,35 @@ def ejercicio5():
     print("--------------")
 
 
+def recursividad_2():
+    def unificar(listas):
+        if len(listas) == 1:
+            return listas[0]
+        else:
+            return listas[0] + unificar(listas[1:])
+
+    listas = [[1, 2, 3], [4, 5, 6], [7], [8]]
+    print("--------------")
+    print("Resultado: ", unificar(listas))
+    print("--------------")
+
+
+def recursividad_3():
+    def decidir(lista1, lista2):
+        if len(lista1) == 0 and len(lista2) == 0:
+            return True
+        elif lista1[0] == lista2[0]:
+            return decidir(lista1[1:], lista2[1:])
+        return False
+
+    lista1 = [1, 2, 3, 4, 5, 6, 7, 8]
+    lista2 = [7, 6, 3, 4, 5, 2, 8, 1]
+    print("--------------")
+    print("Resultado: ", decidir(lista1, lista2))
+    print("Resultado: ", decidir(lista1, lista1))
+    print("--------------")
+
+
 def mostrar_menu():
     print("")
     print("Elija el ejercicio que quiere ejecutar")
@@ -90,6 +119,8 @@ def mostrar_menu():
     print("3 - Formatos de intercambio de datos, ejercicio 1: Obtener informacion de estacion")
     print("4 - Formatos de intercambio de datos, ejercicio 2: Calcular estacion con menos bateria")
     print("5 - Recursion, ejercicio 1: Convertir digitos pares/impares")
+    print("6 - Recursion, ejercicio 2: Unificar lista de listas")
+    print("7 - Recursion, ejercicio 3: Listas son iguales?")
     print("0 - Salir")
 
 
@@ -98,14 +129,18 @@ eleccion = int(input())
 
 while eleccion != 0:
     if eleccion == 1:
-        ejercicio1()
+        regex_1()
     if eleccion == 2:
-        ejercicio2()
+        regex_2()
     if eleccion == 3:
-        ejercicio3()
+        intercambio_1()
     if eleccion == 4:
-        ejercicio4()
+        intercambio_2()
     if eleccion == 5:
-        ejercicio5()
+        recursividad_1()
+    if eleccion == 6:
+        recursividad_2()
+    if eleccion == 7:
+        recursividad_3()
     mostrar_menu()
     eleccion = int(input())
